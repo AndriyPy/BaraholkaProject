82f8950daad1a4b71d9b9345f3f188589ed52fb9
Andriyko222 <125126827+Andriyko222@users.noreply.github.com>
2025-03-16 12:23:36 +0200

    4!!!

--- app.py
+++ app.py
@@ -1,51 +1,91 @@
-from flask import Flask, render_template, request, redirect, session
-from model import User, Good, db
+from flask import Flask, render_template, request, redirect, session, flash
+from base import create_db, Session, engine
+from model import User, Product
+from flask_login import login_required
+
 
 app = Flask(__name__)
+app.secret_key = "very_secret_key"
 
-
+@login_required
 @app.get("/")
 def index():
     return render_template("index.html")
 
 
-
-@app.get("/sign-up")
+@app.get("/signup")
 def registration():
     return render_template("register.html")
 
-@app.post("/sign-up")
-def registration():
-    name = request.form['name']
-    email = request.form['email']
-    password = request.form['password']
-
-    new_user = User(name=name, email=email, password=password)
-    db.session.add(new_user)
-    db.session.commit()
-    db.session.close()
-    return redirect("index")
+@app.post("/signup")
+def postregistration():
+    session = Session()
+    name = request.form.get('name')
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        new_user = User(email, name, password)
+        session.add(new_user)
+        session.commit()
+        session.close()
+        return redirect("/")
+    except Exception as exception:
+        session.rollback()  #session.rollback() — це метод, який використовується в бібліотеці SQLAlchemy для скасування поточної транзакції та повернення до стану, який був до початку цієї транзакції.
+        flash(f"Error: {exception}", "danger")
+    finally:
+        session.close()
+    return render_template("register.html")
 
 
 @app.get("/login")
 def login():
     return render_template("login.html")
 
+
 @app.post("/login")
-def login():
-    email = request.form['email']
-    password = request.form['password']
-
-    user = User.query.filter_by(email=email).first() #пошук за email
+def postlogin():
+    db_session = Session()
+    email = request.form.get('email')
+    password = request.form.get('password')
+    user = db_session.execute(User.query.filter_by(email=email).statement).scalars().first()  # Пошук юзера за email
     if user and user.check_password(password):
-        session['name'] = user.name
-        session['email'] = user.email
-        session['password'] = user.password
-        return redirect("index")
+        db_session["user_id"] = user.id
+        db_session["name"] = user.name
+        db_session["email"] = user.email
+        flash("Вхід виконано успішно!", "success")
+        return redirect("/")
     else:
+        flash("Неправильний email або пароль")
         return render_template("login.html")
+    db_session.close()
 
+
+
+@app.get("/add_good")
+def add_good():
+    return render_template("good.html")
+
+
+@app.post("/add_good")
+def postadd_good():
+    db_session = Session()
+
+    name = request.form.get('name')
+    description = request.form.get('description')
+    price = request.form.get('price')
+    photo = request.form.get('photo')
+    new_product = Product(name=name, description=description, price=price, photo=photo)
+    try:
+        db_session.add(new_product)
+        db_session.commit()
+        db_session.close()
+    except Exception as exception:
+        db_session.rollback()  # session.rollback() — це метод, який використовується в бібліотеці SQLAlchemy для скасування поточної транзакції та повернення до стану, який був до початку цієї транзакції.
+        flash(f"Error: {exception}", "danger")
+    finally:
+        db_session.close()
 
 
 if __name__ == "__main__":
+    create_db()
     app.run(debug=True, port=9900)
--- model.py
+++ model.py
@@ -1,37 +1,46 @@
-from flask_sqlalchemy import SQLAlchemy
-from app import app
+from sqlalchemy import (
+    String
+)
+from sqlalchemy.orm import (
+    Mapped,
+    mapped_column,
+)
+from base import Base, create_db
 import bcrypt
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.baraholka'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
 
+class User(Base):
+    __tablename__ = "users"
+    __table_args__ = {"extend_existing": True}
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=True)
-    email = db.Column(db.String(50), unique=True)
-    password = db.Column(db.String(500), nullable=True)
-    goods = db.relationship('Goods', backref='owner', lazy=True)#зв'язок товару та юзера
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    email: Mapped[str] = mapped_column(String(240), nullable=False)
+    password: Mapped[str] = mapped_column(nullable=False)
 
     def __init__(self, email, name, password):
         self.name = name
         self.email = email
-        self.password = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+        self.password = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+
 
     def check_password(self, password):
         return bcrypt.checkpw(password.encode(), self.password.encode())
 
 
-class Good(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=True)
-    price = db.Column(db.Integer, nullable=True)
-    description = db.Column(db.String(500))
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id')) #зв'язок товару та юзера
+class Product(Base):
+    __tablename__ = "product"
+    __table_args__ = {"extend_existing": True}
 
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    description: Mapped[str] = mapped_column(String(240), nullable=False)
+    price: Mapped[float] = mapped_column(nullable=False)
+    image: Mapped[str] = mapped_column(String(240), nullable=True)
 
-with app.app_context():
-    db.create_all()
-    print("База даних створена!")
+    def __str__(self):
+        return self.name.capitalize()
+
+    def __repr__(self):
+        return f"<Product(id={self.id}, name='{self.name}', price={self.price})>"
